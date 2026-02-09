88b5570fbc0f29bea25408f390a1ae3358fce065
Josip Tomurad <tomurad.josip@gmail.com>
2026-02-09 11:19:12 +0100

    remove cursor from persistence

--- modules/persistence.nix
+++ modules/persistence.nix
@@ -22,8 +22,8 @@
       # User-specific directories
       "/home/${secrets.username}/.config" # Application configurations
       "/home/${secrets.username}/.ssh" # SSH keys and configuration
-      "/home/${secrets.username}/.cursor" # Cursor IDE cache and settings
-      "/home/${secrets.username}/.cursor-server" # Cursor server files
+      # "/home/${secrets.username}/.cursor" # Cursor IDE cache and settings
+      # "/home/${secrets.username}/.cursor-server" # Cursor server files
       "/home/${secrets.username}/nixosconfig" # NixOS configuration
       "/home/${secrets.username}/.config/containers/systemd" # Container systemd units
       "/home/${secrets.username}/.config/systemd/user" # User systemd units

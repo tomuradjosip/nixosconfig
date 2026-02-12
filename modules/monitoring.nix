700bdca5a72b33870a4b249c9d208a673da7abe3
Josip Tomurad <code.channel325@slmail.me>
2026-02-12 01:38:35 +0100

    reduce glances polling rate

--- modules/monitoring.nix
+++ modules/monitoring.nix
@@ -14,8 +14,12 @@
   };
   users.groups.glances = { };
 
-  # Hardware sensor support (loads kernel modules like coretemp, nct6775, etc.)
-  # Run 'sudo sensors-detect' once after first boot to identify available sensors
+  # Hardware sensor kernel modules (detected via sensors-detect)
+  # coretemp: Intel CPU thermal sensor
+  # nct6775: Nuvoton NCT6798D Super I/O (motherboard temps, fan speeds, voltages)
+  boot.kernelModules = [ "coretemp" "nct6775" ];
+
+  # Hardware monitoring tools
   environment.systemPackages = with pkgs; [
     lm_sensors # CLI: run 'sensors' to see temps/fan speeds
     glances # CLI: run 'glances' for a quick overview
@@ -27,7 +31,7 @@
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.glances}/bin/glances -w -B 0.0.0.0 -p 61208 --disable-plugin now";
+      ExecStart = "${pkgs.glances}/bin/glances -w -B 0.0.0.0 -p 61208 -t 5 --disable-plugin now";
       Restart = "on-failure";
       RestartSec = 5;
 
